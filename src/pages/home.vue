72755c2a289b3df4d9eab8d0108c2c925d901bd0
psyche0401 <asq3456922@126.com>
2018-12-27 12:01:51 +0800

    first

---
+++ src/pages/home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div >
+   home
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
